d60e79bf271bae74953ad2552955b1e6a785b5e2
Roxve <116169309+Roxve@users.noreply.github.com>
2025-01-29 13:30:39 +0200

    [main CommandWindow]: commands

--- src/arut/command.go
+++ src/arut/command.go
@@ -12,6 +12,15 @@ type CommandWindow struct {
 	EditorWindow
 }
 
+var commands = map[string]func(wm *ui.WindowManager, args []string){
+	"quit": func(wm *ui.WindowManager, args []string) {
+		// TODO: rethink this design choice
+		// if for example we have an unsaved buffer in another window...
+		wm.Fini()
+		os.Exit(0)
+	},
+}
+
 func newCommandWindow(wm *ui.WindowManager, x, y int) *CommandWindow {
 	inner := newEditorWindow(wm, 50, 15, x, y)
 	return &CommandWindow{
@@ -19,19 +28,41 @@ func newCommandWindow(wm *ui.WindowManager, x, y int) *CommandWindow {
 	}
 }
 
-func (w *CommandWindow) getCommand() string {
-	return w.content[0]
+func (w *CommandWindow) getCommand() []string {
+	command := w.content[0]
+	command = strings.TrimSpace(command)
+
+	return strings.Split(w.content[0], " ")
 }
 
+func (w *CommandWindow) reset(message string) {
+	w.content = []string{"", message}
+	w.cursorX = 0
+	w.cursorY = 0
+}
 func (w *CommandWindow) executeCommand() {
 	command := w.getCommand()
-	if strings.TrimSpace(command) == "exit" {
-		w.Screen().Fini()
-		os.Exit(0)
+	if len(command) == 0 || command[0] == "" {
+		w.close()
+		return
 	}
+
+	cmd := command[0]
+	args := command[1:]
+
+	for c, f := range commands {
+		if strings.Contains(strings.ToLower(c), strings.ToLower(cmd)) {
+			f(w.wm, args)
+			return
+		}
+	}
+
+	w.reset("Command not found")
 }
 
 func (w *CommandWindow) close() {
+	// TODO: rethink this design choice
+	// close has to be overrided or it panics
 	w.wm.Close(w)
 }
 
